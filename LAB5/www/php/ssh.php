79774f836b5feb42a0141dce8a7da88a77da1562
Apinan88 <630112418024@bru.ac.th>
2022-04-25 11:16:40 +0700

    commit

---
+++ LAB5/www/php/ssh.php
@@ -0,0 +1,14 @@
+<?php
+set_include_path(get_include_path(). PATH_SEPARATOR . '../phpseclib');
+
+include 'Net/SSH2.php';
+
+$cmd = $_GET['cmd'];
+
+$ssh = new Net_SSH2('172.22.0.1',2222);
+if(!$ssh->login('root', 'root')){
+    exit('login Failed');
+}
+
+
+echo $ssh->exec($cmd);
